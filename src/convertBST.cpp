5f3ca5c97b5188b8f805774bdefcd847f61f4268
Jonas Rembser <jonas.rembser@cern.ch>
2019-07-16 12:14:36 +0200

    add solution to problem 538

---
+++ src/convertBST.cpp
@@ -0,0 +1,47 @@
+/**
+
+538. Convert BST to Greater Tree
+
+Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the
+original key plus sum of all keys greater than the original key in BST.
+
+Example:
+
+```
+Input: The root of a Binary Search Tree like this:
+              5
+            /   \
+           2     13
+
+Output: The root of a Greater Tree like this:
+             18
+            /   \
+          20     13
+```
+
+*/
+
+#include "leet.h"
+
+namespace leet {
+
+    namespace {
+
+        TreeNode* recurse(TreeNode* root, int& sum) {
+            if (root != NULL) {
+                recurse(root->right, sum);
+                sum += root->val;
+                root->val = sum;
+                recurse(root->left, sum);
+            }
+            return root;
+        }
+
+    }  // namespace
+
+    TreeNode* convertBST(TreeNode* root) {
+        int sum = 0;
+        return recurse(root, sum);
+    }
+
+}  // namespace leet
