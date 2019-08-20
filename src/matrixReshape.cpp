b45c9d6ea4153c65333b8b7f2e67c0d108adb5d6
Jonas Rembser <jonas.rembser@cern.ch>
2019-08-20 12:20:59 +0200

    add solution to problem 566

---
+++ src/matrixReshape.cpp
@@ -0,0 +1,84 @@
+/**
+
+566. Reshape the Matrix
+
+In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different
+size but keep its original data.
+
+You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row
+number and column number of the wanted reshaped matrix, respectively.
+
+The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as
+they were.
+
+If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise,
+output the original matrix.
+
+Example 1:
+
+```
+Input: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+Output: 
+[[1,2,3,4]]
+Explanation:
+The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the
+previous list.
+```
+
+Example 2:
+
+```
+Input: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+Output: 
+[[1,2],
+ [3,4]]
+Explanation:
+There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+```
+
+Note:
+
+ 1. The height and width of the given matrix is in range [1, 100].
+ 2. The given r and c are all positive.
+
+
+*/
+
+#include "leet.h"
+
+namespace leet {
+
+    using namespace std;
+
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        vector<vector<int>> out(r, vector<int>(c));
+
+        if (r * c != nums.size() * nums[0].size()) {
+            return nums;
+        }
+
+        int i = 0;
+        int j = 0;
+        for (auto const& row : nums) {
+            for (int n : row) {
+                if (i == c) {
+                    i = 0;
+                    ++j;
+                }
+                out[j][i] = n;
+                ++i;
+            }
+        }
+
+        return out;
+    }
+
+}  // namespace leet
