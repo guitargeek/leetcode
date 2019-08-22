eb8f47744006bfa48217a853d827752467b5ef92
Jonas Rembser <jonas.rembser@cern.ch>
2019-08-22 10:51:23 +0200

    add solution to problem 258

---
+++ src/addDigits.cpp
@@ -0,0 +1,27 @@
+/**
+
+258. Add Digits
+
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+
+```
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+             Since 2 has only one digit, return it.
+```
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+
+*/
+
+#include "leet.h"
+
+namespace leet {
+
+    int addDigits(int num) { return num < 10 ? num : addDigits(addDigits(num / 10) + num % 10); }
+
+}  // namespace leet
